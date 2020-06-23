a62679dab3aad34341003a1107cd366e2df1b0f6
TFGK <deee5@naver.com>
2020-06-24 00:37:35 +0900

    민석이형 오네가이

--- server/app/Task.php
+++ server/app/Task.php
@@ -6,9 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-<<<<<<< HEAD
-    protected $fillable = ['title', 'content', 'category'];
-=======
     protected $fillable = ['title', 'content', "category"];
->>>>>>> 898c976dcf7f03759cc40225f2d10eef7555e9b5
 }
