7949ed79da6708925ab939f13c1b7e25ebf577dd
TFGK <deee5@naver.com>
2020-06-27 18:27:59 +0900

    1

--- server/database/migrations/2020_06_27_092731_create_timelines_table.php
+++ server/database/migrations/2020_06_27_092731_create_timelines_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTimelineTable extends Migration
+class CreateTimelinesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -30,6 +30,6 @@ class CreateTimelineTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('timeline');
+        Schema::dropIfExists('timelines');
     }
 }
